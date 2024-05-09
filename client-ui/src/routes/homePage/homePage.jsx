a5177ee1b7c5daa7d9c8ced6ba0308932d154aa2
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-05-10 02:19:43 +0600

    Update homePage.jsx

--- client-ui/src/routes/homePage/homePage.jsx
+++ client-ui/src/routes/homePage/homePage.jsx
@@ -1,7 +1,41 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 
 const HomePage = () => {
-  return <div className="homePage">HomePage</div>;
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="tittle">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a
+            gravida elit. Quisque quis lacinia lacus, et placerat nibh. Aenean
+            at purus ipsum. Maecenas id arcu eu elit accumsan luctus. Morbi
+            dignissim condimentum ipsum. Nulla quis lectus accumsan, egestas
+            augue sit amet, vehicula ex.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
