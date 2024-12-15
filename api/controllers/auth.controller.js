08df1de666c7d38e9f170c2e23d28ae90e2522e8
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-12-15 23:32:46 +0600

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,9 +28,32 @@ export const register = async (req, res) => {
 
     }
 };
-export const login = (req, res) => {
-    //db operations
-    console.log("login endpoint");
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        // CHECK IF THE USER EXISTS
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+        console.log(user);
+        if (!user) {
+            return res.status(401).json({ message: "Invalid Credentials" });
+
+        }
+        //CHECK IF THE PASSWORD IS CORRECT
+        // console.log(user);
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(401).json({ message: "Invalid Credentials!" });
+
+        //GENERATE COOKIE TOKEN AND SEND TO THE USER
+        res.setHeader("Set-Cookie", "test=" + "myvalue").json({ message: "Success" })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Failed to login!" })
+    }
 }
 export const logout = (req, res) => {
     //db operations
