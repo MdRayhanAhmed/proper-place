32b9b2d98ac4291f3617fe6ca7617d18cc4d1a17
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-12-21 22:07:04 +0600

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 export const register = async (req, res) => {
     //db operations
@@ -35,7 +36,7 @@ export const login = async (req, res) => {
         const user = await prisma.user.findUnique({
             where: { username }
         })
-        console.log(user);
+        // console.log(user);
         if (!user) {
             return res.status(401).json({ message: "Invalid Credentials" });
 
@@ -48,7 +49,20 @@ export const login = async (req, res) => {
         if (!isPasswordValid) return res.status(401).json({ message: "Invalid Credentials!" });
 
         //GENERATE COOKIE TOKEN AND SEND TO THE USER
-        res.setHeader("Set-Cookie", "test=" + "myvalue").json({ message: "Success" })
+        // res.setHeader("Set-Cookie", "test=" + "myvalue").json({ message: "Success" })
+        const age = 1000 * 60 * 60 * 24 * 7;
+        const token = jwt.sign({
+            id: user.id
+        }, process.env.JWT_SECRET_KEY, { expiresIn: age })
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure: true
+            maxAge: age
+
+        })
+            .status(200)
+            .json({ message: "Login Successful" });
 
     } catch (error) {
         console.log(error);
@@ -58,4 +72,5 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
     //db operations
     console.log("logout endpoint");
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 }
