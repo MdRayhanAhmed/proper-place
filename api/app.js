fdbf8ae949285e43dc0a7ed324bcd80377ebbf6f
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-12-21 22:06:52 +0600

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 // app.use("/api/auth/register", (req, res) => {
@@ -30,5 +31,5 @@ app.listen(8800, () => {
 
 })
 
-console.log("test1");
+// console.log("test1");
 
