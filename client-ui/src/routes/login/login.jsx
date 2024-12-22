6ae32215ce5f2d076c774aa1cd76d3e7c13cbb01
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-12-22 14:15:17 +0600

    Update login.jsx

--- client-ui/src/routes/login/login.jsx
+++ client-ui/src/routes/login/login.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
 import "./login.scss";
 
 function Login() {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <form action="">
+          <h1>Welcome Back</h1>
+          <input
+            type="text"
+            name="username"
+            required
+            minLength={3}
+            maxLength={20}
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Password"
+          />
+          <button>Login</button>
+          <Link to="/register">Don't you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
