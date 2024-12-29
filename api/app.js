4c18b883e8e8ef69553a7f8932af6cbb5b5318a4
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2024-12-29 23:39:15 +0600

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
 // app.use("/api/auth/register", (req, res) => {
 //     res.send("It works register")
 // })
