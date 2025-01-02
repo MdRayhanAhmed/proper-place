d1b58fe64dc19d080a5d90ca205127bde1212d35
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2025-01-02 23:51:20 +0600

    Update test.route.js

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { shouldBeAdmin, shouldBeLoggedIn } from "../controllers/test.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import { shouldBeAdminToken } from "../middleware/shouldBeAdminToken.js"
 
 const router = express.Router()
 
 router.get("/should-be-logged-in", verifyToken, shouldBeLoggedIn);
 
-router.get("/should-be-admin", shouldBeAdmin);
+router.get("/should-be-admin", shouldBeAdminToken, shouldBeAdmin);
 
 export default router;
 
